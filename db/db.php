97c14cc627dbbe088d843dffc24f4e932e3c2fc8
VeronicaFontana <fontana.veronica98@gmail.com>
2023-11-07 21:13:16 +0100

    [ADD] Cards, style

--- db/db.php
+++ db/db.php
@@ -3,11 +3,11 @@
 
 
 $productions = [
-  new Movie(142, "The Avengers", "action", ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson", "Jeremy Renner"], new Media("avengers.jpg", "The Avengers")),
+  new Movie(142, "The Avengers", "Azione", ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson", "Jeremy Renner"], new Media("avengers.jpg", "The Avengers")),
   new TvSerie(9, 147, "CSI: New York", "Poliziesco", ["Gary Sinise", "Melina Kanakaredes", "Carmine Giovinazzo", "Hill Harper", "Eddie Cahill"], new Media("csi.jpg", "CSI")),
-  new Movie(105, "The Grinch", "comedy", ["Jim Carrey", "Taylor Momsen", "Bill Irwin", "Christine Baranski"], new Media("grinch.jpg", "The Grinch")),
+  new Movie(105, "The Grinch", "Commedia", ["Jim Carrey", "Taylor Momsen", "Bill Irwin", "Christine Baranski"], new Media("grinch.jpg", "The Grinch")),
   new TvSerie(2, 16, "Bridgerton", "Sentimentale", ["Adjoa Andoh", "Lorraine Ashbourne", "Jonathan Bailey", "Ruby Barker","Phoebe Dynevor"], new Media("bridgerton.jpg", "Bridgerton")),
-  new Movie(135, "It", "horror", ["Bill Skarsgård", "Jaeden Martell", "Sophia Lillis", "Finn Wolfhard", "Jack Dylan Grazer"], new Media("it.jpg", "It")),
+  new Movie(135, "It", "Horror", ["Bill Skarsgård", "Jaeden Martell", "Sophia Lillis", "Finn Wolfhard", "Jack Dylan Grazer"], new Media("it.jpg", "It")),
   new TvSerie(6, 127, "Young Sheldon", "Sitcom", ["Iain Armitage", "Zoe Perry", "Lance Barber", "Montana Jordan", "Raegan Revord", "Annie Potts"], new Media("young_sheldon.jpg", "Young Sheldon")),
-  new Movie(169, "Salvate il soldato Ryan", "war", ["Tom Hanks", "Edward Burns", "Matt Damon", "Tom Sizemore", "Vin Diesel"], new Media("salvateilsoldatoryan.jpg", "Salvate il soldato Ryan"))
+  new Movie(169, "Salvate il soldato Ryan", "Guerra", ["Tom Hanks", "Edward Burns", "Matt Damon", "Tom Sizemore", "Vin Diesel"], new Media("salvateilsoldatoryan.jpg", "Salvate il soldato Ryan"))
 ];
--- index.php
+++ index.php
@@ -19,17 +19,21 @@ require_once __DIR__ . "/db/db.php";
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
+  <link rel="stylesheet" href="style.css">  
+
   <title>Productions</title>
 </head>
 <body>
-  <div class="container mt-3 d-flex justify-content-between">
+  <h1 class="text-center mt-3">FILM AND TV SERIES</h1>
+  <div class="container mt-3 d-flex flex-wrap">
     <?php foreach($productions as $production): ?>
       <div class="card" style="width: 18rem;">
-        <img src="img/<?php echo $production->image->file_name ?>" class="card-img-top h-100" alt="<?php echo $production->image->name ?>">
+        <img src="img/<?php echo $production->image->file_name ?>" class="card-img-top" alt="<?php echo $production->image->name ?>">
         <div class="card-body">
           <h5 class="card-title"><?php echo $production->title ?></h5>
           <p class="card-text"><?php echo $production->genre ?></p>
           <p class="card-text"><?php echo implode(" - ", $production->cast) ?></p>
+          <p class="card-text">Tipo: <?php echo get_class($production) ?></p>
         </div>
       </div>
     <?php endforeach; ?>
